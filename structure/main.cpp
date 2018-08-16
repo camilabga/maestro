0f7fb0299003821b469f4f6342b6376c8b6ad03d
camilabga <camilabga@ufrn.edu.br>
2018-08-16 15:03:39 -0300

    run test - new enviroment

--- structure/main.cpp
+++ structure/main.cpp
@@ -23,8 +23,8 @@ int main (int argc, char **argv) {
     trajectory.unnormalize(Point(FRAME_WIDTH/2, FRAME_HEIGHT/2));
     //trajectory.unnormalize(Point(300, 100));
 
-    trajectory.saveMovement("../data/sample02_square_01.csv");
-    vision.record("../data/sample02_square_01.avi");
+    trajectory.saveMovement("../data/random_test.csv");
+    vision.record("../../Videos/random_test.avi");
 
     while(1){
         vision.calculateTagCenter();
